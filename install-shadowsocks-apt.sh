fce4a8e0ff924f969b050aae76a012f1dd1e92c7
Yuhui Huang <mrhuangyuhui@users.noreply.github.com>
2018-12-24 16:53:17 +0800

    Update install-shadowsocks-apt.sh

--- install-shadowsocks-apt.sh
+++ install-shadowsocks-apt.sh
@@ -8,4 +8,4 @@ sudo apt-get install -y python-pip git
 pip install git+https://github.com/shadowsocks/shadowsocks.git@master
 
 # 启动
-ssserver -k gz@;8n69=5BR0*d# -d start
+ssserver -k phlox-scream-knit-sabotage -d start
